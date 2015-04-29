4ccceb35035837894969d43f38b05196b03a92ff
vishalanand23 <Helloworld123>
2015-04-28 23:41:47 -0700

    added number picker.

--- app/src/main/java/com/games/vishalanand23/bullsandcowsandroid/PlayActivity.java
+++ app/src/main/java/com/games/vishalanand23/bullsandcowsandroid/PlayActivity.java
@@ -4,11 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.NumberPicker;
 
 
 public class PlayActivity extends AppCompatActivity {
@@ -17,51 +13,41 @@ public class PlayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.number_picker_activity);
+        initializeNumberPicker();
+    }
+
+    private void initializeNumberPicker() {
         switch (numberOfDigits) {
             case 2:
-                initialize(
-                        (Spinner) findViewById(R.id.digit_1),
-                        (Spinner) findViewById(R.id.digit_2));
+                initializeNumberPickerArray(
+                        (NumberPicker) findViewById(R.id.digit_1),
+                        (NumberPicker) findViewById(R.id.digit_2));
                 break;
             case 3:
-                initialize(
-                        (Spinner) findViewById(R.id.digit_1),
-                        (Spinner) findViewById(R.id.digit_2),
-                        (Spinner) findViewById(R.id.digit_3));
+                initializeNumberPickerArray(
+                        (NumberPicker) findViewById(R.id.digit_1),
+                        (NumberPicker) findViewById(R.id.digit_2),
+                        (NumberPicker) findViewById(R.id.digit_3));
                 break;
             case 4:
-                initialize(
-                        (Spinner) findViewById(R.id.digit_1),
-                        (Spinner) findViewById(R.id.digit_2),
-                        (Spinner) findViewById(R.id.digit_3),
-                        (Spinner) findViewById(R.id.digit_4));
+                initializeNumberPickerArray(
+                        (NumberPicker) findViewById(R.id.digit_1),
+                        (NumberPicker) findViewById(R.id.digit_2),
+                        (NumberPicker) findViewById(R.id.digit_3),
+                        (NumberPicker) findViewById(R.id.digit_4));
                 break;
             default:
 
         }
-        setContentView(R.layout.activity_game);
     }
 
-    private void initialize(Spinner... spinnerArray) {
-        for (final Spinner spinner : spinnerArray) {
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                    R.array.digit_array, android.R.layout.simple_spinner_item);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-            spinner.setAdapter(adapter);
-            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
-                @Override
-                public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
-                    String ss = spinner.getSelectedItem().toString();
-                    Toast.makeText(getBaseContext(), ss, Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onNothingSelected(AdapterView<?> arg0) {
-                }
-
-            });
+    private void initializeNumberPickerArray(NumberPicker... spinnerArray) {
+        for (final NumberPicker np : spinnerArray) {
+            np.setMinValue(0);
+            np.setMaxValue(9);
+            np.setWrapSelectorWheel(true);
         }
     }
 
